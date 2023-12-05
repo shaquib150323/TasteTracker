c06a306c5e3abfb26c8a7ca3f2acd0f234634898
Shaquib Hussain <shaquibhussain2001@gmail.com>
2023-12-06 01:32:36 +0530

    initial

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,17 @@ const app = express()
 const port = 8080
 const mongoDB= require('./db');
 mongoDB();
-app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","https://stellar-shortbread-5121bf.netlify.app/");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-type, Accept"
-  );
-  next();
-})
+// app.use((req,res,next)=>{
+//   res.setHeader("Access-Control-Allow-Origin","https://stellar-shortbread-5121bf.netlify.app/");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-type, Accept"
+//   );
+//   next();
+// })
+
+app.use(cors({origin:"https://stellar-shortbread-5121bf.netlify.app/",Credential:true}))
+
 app.use(express.json())
 app.use('/api',require("./routes/CreateUser"));
 app.use('/api',require("./routes/DisplayData"));
--- backend/routes/DisplayData.js
+++ backend/routes/DisplayData.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 router.post('/foodData',(req,res)=>{
 try{
-    // console.log(global.food_items)
    res.send([global.food_items,global.foodCategory]);
 }catch(error){
 console.error(error.message);
